3365c0700103d0b64a987fa6b99bfafc71a1facd
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-04-24 19:22:50 +0700

    ex Category

--- src/entities/Category.ts
+++ src/entities/Category.ts
@@ -3,7 +3,46 @@
         "categoryName": "ManhBut"
     }
  */
-export default interface Order{
-    categoryId: string;
-    categoryName: string
+import apiRequest from "../Api/ApiRequest";
+export default class Category{
+    //Fields
+    private categoryId: string;
+    private categoryName: string;
+    
+    //Constructors
+    public constructor(categoryId: string, categoryName: string){
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    //Return Json
+    toJson(){
+        return {
+            "categoryId": this.categoryId,
+            "categoryName": this.categoryName
+        }
+    }
+
+    //GetAllCategory
+    public static async getAllCategory(): Promise<Category[] | null>{
+       const res = await apiRequest("GET", "/categories");
+       if(res.status === 200){
+            const data = res.json();
+            return data.map((item: any) => new Category(item.categoryId, item.categoryName));
+       }
+       else{
+            return null
+       }
+    }
+
+    //insertCategory
+    public static async insertCategory(category: Category): Promise<boolean>{
+        const res = await apiRequest('POST', '/categories/add', category.toJson() );
+
+        if(res.status === 200){
+            return true;
+        }
+        return false;
+    }
+    
 }
